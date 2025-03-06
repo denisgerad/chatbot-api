f8400bd6d303d3ffc8e8a493b0b4a6bb7e98cb57
saroshcruz <saroshcruz@gmail.com>
2025-03-06 14:44:53 +0530

    modified

--- app.py
+++ app.py
@@ -25,5 +25,4 @@ def chatbot():
     return jsonify({"response": best_match})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render
-    app.run(host="0.0.0.0", port=port)  # Use Render's assigned PORT
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
