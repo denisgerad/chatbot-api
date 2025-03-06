f4ba9593d104f3a3c29b41f61e63a041b9a4f79a
saroshcruz <saroshcruz@gmail.com>
2025-03-06 16:04:03 +0530

    modified for cors

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import os
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from sentence_transformers import SentenceTransformer, util
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
 
 # Load chatbot model
 model = SentenceTransformer("all-MiniLM-L6-v2")
